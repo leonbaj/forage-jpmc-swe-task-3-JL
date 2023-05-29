7e5b619decc62f128710e4b37a177b6a7796c260
leonbaj <122085077+leonbaj@users.noreply.github.com>
2023-05-29 02:57:31 -0500

    Update dataManipulator.ts interface Row
    set up the interface row to match current Schema, as This file (dataManipulator.ts) is responsible for processing raw stock data.

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,9 +1,16 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  /*row interface similiar set up to the old schema, and since the updated one is in place now, 
+  we must update this interface to match the current schema.
+  */
+  price_abc: number,
+  price_def: number,
+  ratio: number, 
   timestamp: Date,
+  upper_bound: number, 
+  lower_bound: number,
+  trigger_alert: number | undefined, 
 }
 
 
